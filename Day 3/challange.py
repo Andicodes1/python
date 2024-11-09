3e04022be21420cc4d64a4a1d821a1faf670a35a
Studenti <asdasd@gmail.com>
2024-11-09 12:48:05 +0100

    ...

---
+++ Day 3/challange.py
@@ -0,0 +1,13 @@
+#imagine you're a loyalty program for a popular store , you need to check weather a specific customer is for of this program , yoi should grant them the benefits of the discount
+#Memebers of the program:
+#Alice
+#Bob
+#Charlie
+#Anna
+
+costumers = {"Alice", "Bob", "Charlie", "Anna"}
+costumer = "Bobb"
+
+is_member = costumer in costumers
+
+print("This person is in the loyalty program reward: ", is_member)
---
+++ Day 3/conditionals.py
@@ -0,0 +1,18 @@
+#Conditional
+
+age = 18
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+#Temereature
+temprerature = 28
+
+if temprerature > 30:
+    print("Is a hot day today")
+elif 20 <= temprerature <= 30:
+    print("The weather is pleasant")
+else:
+    print("It is a cold day today")
