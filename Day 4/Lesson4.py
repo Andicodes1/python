63a5e9e1023f1acef9c0e879b207ac7ac8c59edb
Studenti <studenti123@gmail.com>
2024-11-16 11:52:48 +0100

    ....

---
+++ Day 4/Lesson4.py
@@ -0,0 +1,13 @@
+#Create a list
+
+names = ["Blerta", "Erosi", "Driloni", "Brikena" , "Ylli"]
+
+#Iterate in the names list and print every name
+
+for name in names:
+    print(name)
+
+
+    ###################
+
+sentence = "Hello World"
